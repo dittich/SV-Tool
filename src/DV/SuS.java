1dc933b639736571b5ff336ed9b2b89d1ecdc089
dittich <sv@dittich.de>
2015-04-25 21:39:39 +0200

    GUI Buttons für Menü ink. Image erzeugt und eingepflegt

---
+++ src/DV/SuS.java
@@ -0,0 +1,105 @@
+package DV;
+
+import java.sql.Blob;
+import java.sql.Timestamp;
+
+public class SuS {
+	private int id, schueler_id;
+	private String name, vorname, geschlecht, klasse, typ;
+	private Blob bild;
+	private Timestamp gebdatum;
+	private boolean geloescht, selektiert;
+	
+	public SuS()
+	{
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getSchueler_id() {
+		return schueler_id;
+	}
+
+	public void setSchueler_id(int schueler_id) {
+		this.schueler_id = schueler_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getGeschlecht() {
+		return geschlecht;
+	}
+
+	public void setGeschlecht(String geschlecht) {
+		this.geschlecht = geschlecht;
+	}
+
+	public String getKlasse() {
+		return klasse;
+	}
+
+	public void setKlasse(String klasse) {
+		this.klasse = klasse;
+	}
+
+	public String getTyp() {
+		return typ;
+	}
+
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public Blob getBild() {
+		return bild;
+	}
+
+	public void setBild(Blob bild) {
+		this.bild = bild;
+	}
+
+	public Timestamp getGebdatum() {
+		return gebdatum;
+	}
+
+	public void setGebdatum(Timestamp gebdatum) {
+		this.gebdatum = gebdatum;
+	}
+
+	public boolean isGeloescht() {
+		return geloescht;
+	}
+
+	public void setGeloescht(boolean geloescht) {
+		this.geloescht = geloescht;
+	}
+
+	public boolean isSelektiert() {
+		return selektiert;
+	}
+
+	public void setSelektiert(boolean selektiert) {
+		this.selektiert = selektiert;
+	}
+}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,43 +1,30 @@
 package test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
+
+import DV.Datenverwaltung;
  
 public class Test {
  
     /**
      * @param args
+     * @throws Exception 
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        try
+        Datenverwaltung dv = new Datenverwaltung();
+        
+        dv.connect();
+        
+        ResultSet rs = dv.sqlQuery("select * from sv_schueler");
+        
+        while(rs.next())
         {
-        	System.out.println("w");
-            Class.forName("org.h2.Driver");
-            System.out.println("w");
-            Connection con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "" );
-            System.out.println("w");
-            Statement stmt = con.createStatement();
-            //stmt.executeUpdate( "DROP TABLE table1" );
-            stmt.executeUpdate( "CREATE TABLE table1 ( user varchar(50) )" );
-            stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Claudio' )" );
-            stmt.executeUpdate( "INSERT INTO table1 ( user ) VALUES ( 'Bernasconi' )" );
- 
-            ResultSet rs = stmt.executeQuery("SELECT * FROM table1");
-            
-            while( rs.next() )
-            {
-                String name = rs.getString("user");
-                System.out.println( name );
-            }
-            stmt.close();
-            con.close();
+        	String name = rs.getString("name");
+        	String vorname = rs.getString("vorname");
+        	System.out.println(name+", "+vorname);
         }
-        catch( Exception e )
-        {
-            System.out.println( e.getMessage() );
-        }
+        
+        dv.close();
     }
 }
