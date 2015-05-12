395c4c67e77d723950a7e9fdb9cf70270d695424
Dittich <SV_Tool@dittich.de>
2015-05-12 21:12:18 +0200

    CSV-Mod 1

--- src/gui/CSVDienste.java
+++ src/gui/CSVDienste.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 import javax.swing.JPanel;
@@ -13,7 +14,11 @@ import svt.SVTool;
 
 public class CSVDienste {
 	
+	private HashMap<String, String> hmDbName;
+	private HashMap<String, Integer> hmCsvIndex;
+	
 	public CSVDienste(){
+		hmDbName = this.generateHmDbName();
 	}
 	
 	public void csvImport(JPanel pnl, File csvFile, SVTool svTool){
@@ -23,18 +28,39 @@ public class CSVDienste {
 		boolean headline = true;
 		
 		try {
-			File fileDir = new File("d:/sus2014.csv");
+			File fileDir = new File("d:/SAusweis.csv");
 			BufferedReader in = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream(fileDir), "UTF8"));
 			String zeile = null;
 			while ((zeile = in.readLine()) != null) {
+				System.out.println(zeile);
 				if(!headline) cutSuS(zeile);
-				else headline=false;
+				else {
+					headline=false;
+					hmCsvIndex = this.generateHmCsvIndex(zeile);
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private HashMap<String, Integer> generateHmCsvIndex(String zeile){
+		zeile = ";"+zeile;
+		System.out.println(zeile);
+		HashMap<String, Integer> lHmCsvIndex = new HashMap<String, Integer>();
+		String[] parts = zeile.split(";");
+		for(int i=0; i<parts.length; i++){
+			System.out.println("Line: "+parts[i]);
+			if(hmDbName.containsKey(parts[i])){
+				String keyTabName = hmDbName.get(parts[i]);
+				lHmCsvIndex.put(keyTabName, i);
+				System.out.println(keyTabName+" - "+ i);
+			}
+			else System.out.println("OUT: "+parts[i]);
+		}
+		return lHmCsvIndex;
 	}
 	
 	private void cutSuS(String zeile){
@@ -55,6 +81,20 @@ public class CSVDienste {
 		
 		SuS sus = new SuS(susID,name,vorname,gebdatum,klasse,geloescht,geschlecht);
 		
-		System.out.println(sus.toString());
+		//System.out.println(sus.toString());
+	}
+	
+	private HashMap generateHmDbName(){
+		// "Interne ID-Nummer";"Nachname";"Vorname";"Geburtsdatum";"Klasse";"Gelöscht?";"Geschlecht"
+		HashMap<String, String> lDbName = new HashMap<String, String>();
+		lDbName.put("\"Interne ID-Nummer\"", "schueler_id");
+		lDbName.put("\"Nachname\"","name");
+		lDbName.put("\"Vorname\"","vorname");
+		lDbName.put("\"Geburtsdatum\"","gebdatum");
+		lDbName.put("\"Klasse\"","klasse");
+		lDbName.put("\"Gelöscht?\"","geloescht");
+		lDbName.put("\"Geschlecht\"","geschlecht");
+		
+		return lDbName;	
 	}
 }
