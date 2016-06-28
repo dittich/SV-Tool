7b046d4a23ef325d1f1083f39abd46b8558c1181
dittich <sv@dittich.de>
2016-06-28 17:09:33 +0200

    New Config-Datei

---
+++ src/svt/Config.java
@@ -0,0 +1,57 @@
+package svt;
+
+public class Config {
+	
+	private String dbIp = "localhost";
+	private int dbPort = 3306;
+	
+	private String dbName = "sv_ausweise";
+	
+	private String dbUser = "sv";
+	private String dbPassword = "sv";
+	
+	public Config()
+	{
+		
+	}
+
+	public String getDbIp() {
+		return dbIp;
+	}
+
+	public void setDbIp(String dbIp) {
+		this.dbIp = dbIp;
+	}
+
+	public int getDbPort() {
+		return dbPort;
+	}
+
+	public void setDbPort(int dbPort) {
+		this.dbPort = dbPort;
+	}
+
+	public String getDbName() {
+		return dbName;
+	}
+
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+
+	public String getDbUser() {
+		return dbUser;
+	}
+
+	public void setDbUser(String dbUser) {
+		this.dbUser = dbUser;
+	}
+
+	public String getDbPassword() {
+		return dbPassword;
+	}
+
+	public void setDbPassword(String dbPassword) {
+		this.dbPassword = dbPassword;
+	}
+}
